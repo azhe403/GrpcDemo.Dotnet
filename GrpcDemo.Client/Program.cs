85ddc0ea311ba6ee70e13e3d1267fdd122c95f80
Azhe Kun <azhe403@gmail.com>
2024-07-29 22:52:27 +0700

    git init

---
+++ GrpcDemo.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcDemo.Client;
+
+Console.WriteLine("Hello, World!");
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7005");
+
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
